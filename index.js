73fdd9c0235642c6bb4ae2f6a3109f21311d1a76
hassanyoussef50802025-cmd <hassanyoussef50802025@gmail.com>
2026-09-11 03:05:00 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
 import { AppRegistry } from 'react-native';
 import App from './App';
 import { name as appName } from './app.json';
-import BackgroundFetch from './BackgroundService';
-
-// تسجيل مهمة الخلفية للإشعارات
-AppRegistry.registerHeadlessTask('BariqBackgroundTask', () => BackgroundFetch);
 
 AppRegistry.registerComponent(appName, () => App);
